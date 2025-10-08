a201b1e48422a12749845baa3c57973530cb55a5
manish2802 <manishverma.java@gmail.com>
2025-10-08 15:09:42 +0530

    ELSP

--- apps/admin-portal-app/src/app/app.config.ts
+++ apps/admin-portal-app/src/app/app.config.ts
@@ -3,13 +3,23 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { provideRouter, Routes } from '@angular/router';
+import { HomePageComponent } from './features/home/home-page';
+import { LoginPageComponent } from './features/login/login-page';
+import { RegisterPageComponent } from './features/register/register-page';
+import { LandingPageComponent } from './features/landing/landing-page';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(routes),
   ],
 };
